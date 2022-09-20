1841efd6d02b40c5e1cc79294abe3dc941845dc4
Wallace Tácio <wallacett.dev@gmail.com>
2022-09-20 11:55:47 -0300

    Adiciona solução ao exercício Múltiplo de 3 e 7 FATEC 2022/2

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Digite um número: ");
+
+double num = Convert.ToDouble(Console.ReadLine());
+
+if(num%3 == 0 && num%7 == 0){
+    Console.WriteLine($"O número {num} é múltiplo de 3 e 7.");
+} else {
+    Console.WriteLine($"{num} não é múltiplo.");
+}
+Console.ReadKey();
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # multiplo7e5
 Exercício Múltiplo de 7 e 5
+
+Crie um programa que permite que o usuário digite o valor de um numero qualquer, verificar se o numero é múltiplo de 3 e de 7 ao mesmo tempo, caso seja imprimir na tela “Múltiplo de 3 e 7”, caso não imprimir “Não é múltiplo”.
